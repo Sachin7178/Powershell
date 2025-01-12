53a1d7a154af9fa14b76c81d798a682e0551b36a
Sachin7178 <159446555+Sachin7178@users.noreply.github.com>
2025-01-12 20:16:12 +0530

    Adding az login, OS build info, Inventory

---
+++ LoginToAzuresteps.ps1
@@ -0,0 +1,40 @@
+﻿
+Install-Module -NAme AZ -Scope CurrentUser -Repository PSGallery
+#----------------------
+az login --use-device-code
+#Connect-AzAccount -SubscriptionName 'mbr_workshop'
+
+az account show
+
+
+az account list --output table
+
+
+
+
+
+az login --tenant "0938ad44-73dc-4627-b0ab-cb7ad38dec7a"  #my subscription
+az account set --subscription "2ae3b17d-2520-4336-9e0d-adf658bb945c"
+
+az login --tenant 0938ad44-73dc-4627-b0ab-cb7ad38dec7a --use-device-code # mimp
+
+az login --tenantID 0938ad44-73dc-4627-b0ab-cb7ad38dec7a
+
+az group create --name MyResourceGroup --location eastus
+az group delete --name MyResourceGroup --yes --no-wait
+
+
+#-----------------
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Update-AzConfig -EnableLoginByWam $false
+
+
+Connect-AzAccount -TenantId 0938ad44-73dc-4627-b0ab-cb7ad38dec7a -UseDeviceAuthentication #alternate
+Connect-AzAccount -UseDeviceAuthentication
+
+Get-AzContext
+Get-AzSubscription
+Set-AzContext -Subscription "2ae3b17d-2520-4336-9e0d-adf658bb945c"
+
+New-AzResourceGroup -Name MyResource -Location eastus
+                        
---
+++ VMinventory.ps1
@@ -0,0 +1,81 @@
+﻿# (Get-AzNetworkInterface -ResourceGroupName $vm.ResourceGroupName -Name (Split-Path -Leaf ($vm.NetworkProfile.NetworkInterfaces[0].Id))).IpConfigurations[0].PrivateIpAddress
+
+$Myvms = Get-AzVM
+
+$Properties = @()
+
+foreach ($vm in $Myvms) {
+    
+    $privateIpAddress = ""
+    $resourceStatus = ""
+    $osPublisher = ""
+    $osOffer = ""
+    $description = ""
+    $environment = ""
+    $serverBanding = ""
+    $complianceFlag = ""
+
+
+    if ($vm.NetworkProfile.NetworkInterfaces -and $vm.NetworkProfile.NetworkInterfaces.Count -gt 0) {
+        $nicId = $vm.NetworkProfile.NetworkInterfaces[0].Id
+        $nicName = Split-Path -Leaf $nicId
+        $nic = Get-AzNetworkInterface -ResourceGroupName $vm.ResourceGroupName -Name $nicName
+
+        if ($nic.IpConfigurations -and $nic.IpConfigurations.Count -gt 0) {
+            $privateIpAddress = $nic.IpConfigurations[0].PrivateIpAddress
+        }
+    }
+
+    $powerState = (Get-AzVM -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name -Status).Statuses | Where-Object Code -like 'PowerState/*' | Select-Object -ExpandProperty DisplayStatus
+    $provisioningState = $vm.ProvisioningState
+    
+    if ($powerState -eq "VM running" -and $provisioningState -eq "Succeeded") {
+        $resourceStatus = "Provisioning succeeded VM running"
+    } elseif ($powerState -eq "VM deallocated") {
+        $resourceStatus = "Provisioning succeeded VM deallocated"
+    } else {
+        $resourceStatus = "Unknown"
+    }
+
+
+    if ($vm.StorageProfile.ImageReference) {
+        $osPublisher = $vm.StorageProfile.ImageReference.Publisher
+        $osOffer = $vm.StorageProfile.ImageReference.Sku
+    }
+
+    if ($vm.Tags) {
+        if ($vm.Tags.Description) { $description = $vm.Tags.Description } else {$description = ""}
+        if ($vm.Tags.Environment) { $environment = $vm.Tags.Environment } else {$environment = ""}
+        if ($vm.Tags.ServerBanding) { $serverBanding = $vm.Tags.ServerBanding } else {$serverBanding = ""}
+        if ($vm.Tags.Compliance) { $complianceFlag = $vm.Tags.Compliance } else {$complianceFlag = ""}
+    }
+
+    $vmInfo = @{
+        "ComplianceFlag" = $complianceFlag
+        "ServerBanding"  = $serverBanding
+        "Environment"    = $environment
+        "Description"    = $description
+        "VM_Size"        = $vm.HardwareProfile.VmSize
+        "OS_Offer"       = $osOffer
+        "OS_Publisher"   = if ($osPublisher -eq "MicrosoftWindowsServer") { "WindowsServer" } elseif ($osPublisher -eq "RedHat") { "RHEL" } else { $osPublisher }
+        "OS_Flavor"      = $osPublisher
+        "OS"             = $vm.StorageProfile.OsDisk.OsType
+        "Location"       = $vm.Location
+        "ResourceGroup"  = $vm.ResourceGroupName
+        "ResourceStatus" = $resourceStatus
+        "PrivateIP"      = $privateIpAddress
+        "ResourceName"   = $vm.Name
+        "ResourceType"   = "VirtualMachine"
+        
+    }
+
+    $Properties += [PSCustomObject]$vmInfo
+}
+
+
+#$Properties = $Properties | Sort-Object -Property ResourceName
+$Properties | Export-Excel -Path "C:\Users\sachin.dilip.patil\Downloads\File.xlsx" -AutoSize -FreezeTopRow -BoldTopRow
+
+
+
+#$Properties | Format-Table -AutoSize
